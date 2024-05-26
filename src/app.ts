295d81db22f21ee334fbb2a5d7a05f2d0c68ff5c
nhat8002nguyen <nv.nhat8002@gmail.com>
2024-05-26 14:56:10 +0700

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import express from "express"
 import bodyParser from "body-parser"
 import routes from "./routes"
+import cors from "cors"
+
 const app = express()
 require("dotenv").config()
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 
 app.use(bodyParser.json())
 app.use("/api", routes)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 9000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
